a9cfa0643f0f0462ee262062b6e8a312eab67a7b
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-07-25 12:11:43 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/row_and_column_widget_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.row_and_column_widget_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+   return MaterialApp(
+     title: "General Widget(Column and Row)",
+     theme: ThemeData(
+       primarySwatch: Colors.blue,
+     ),
+     home: FirstScreen(),
+   );
+  }
+
+}
+
+class FirstScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+          onPressed: () {
+            // action menu
+          },
+        ),
+
+        title: Text("First Screen"),
+        
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              //action search
+            },
+          )
+        ],
+      ),
+
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          IconButton(icon: Icon(Icons.share), onPressed: () {  },),
+          IconButton(icon: Icon(Icons.thumb_up), onPressed: () {  },),
+          IconButton(icon: Icon(Icons.thumb_down), onPressed: () {  },),
+        ],
+      ),
+    );
+  }
+}
